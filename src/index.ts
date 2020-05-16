805b1a988416af204cb32d24345f69b465bf125c
HyunnoN <hyunnoh01@gmail.com>
2020-05-17 08:26:43 +0900

    Add feature

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,41 @@
 import Discord from 'discord.js'
+import dotenv from 'dotenv'
+
 
 const client = new Discord.Client();
 
 client.on('message', msg => {
-  if (msg.author.id === client.user?.id) return;
+  // if (msg.author.id === client.user?.id || msg.author.bot) return;
 
+
+  // if (msg.content.search(/어떻게/) !== -1) {
+  //   console.log(msg.content);
+  //   msg.channel.send(`> ${msg.content}\n${msg.author} Fun하고 Cool하고 Sexy하게 해야 합니다.`)
+  // }
+  // else if (msg.content.search(/['왜'|'?']/) !== -1) {
+  //   console.log(msg.content)
+  //   msg.reply('그걸 설명하는 것 자체가 섹시하지 않네요')
+  // } else if (msg.content.search(/(하고)?''|' '싶다/) !== -1) {
+  //   console.log(msg.content)
+  //   msg.channel.send(`@${msg.author.username} ~하는 것은, ~하는 것입니다`)
+  // }
+  if (msg.author.id === client.user?.id || msg.author.bot) return;
+
+  if (msg.content.search(/[켜져|살아]있냐/) !== -1) {
+    msg.channel.send('저는 죽지 않았습니다. 왜냐하면 살아있기 때문입니다.')
+  }
+  if (msg.content.search(/할거("?"|임"?"?)/) !== -1) {
+    msg.channel.send(`<@${msg.author.id}> 하겠습니다. 그것이 약속이니까`)
+  }
   if (msg.content.search(/어떻게/) !== -1) {
-    msg.channel.send(`> ${msg.content}\n${msg.author} Fun하고 Cool하고 Sexy하게 해야 합니다.`);
+    msg.channel.send(`> ${msg.content}\n<@${msg.author.id}> Fun하고 Cool하고 Sexy하게 해야 합니다.`)
   }
   else if (msg.content.search(/['왜'|'?']/) !== -1) {
-    console.log(msg.content)
-    msg.reply('그걸 설명하는 것 자체가 섹시하지 않네요')
-  } else if (msg.content.search(/(하고)?''|' '싶다/) !== -1) {
-    console.log(msg.content)
-    msg.channel.send(`@${msg.author.username} ~하는 것은, ~하는 것입니다`)
+    msg.channel.send(`> ${msg.content}\n<@${msg.author.id}> 그걸 설명하는 것 자체가 섹시하지 않네요.`)
   }
+
 });
 
-client.login('NzA5MDAwNzM3NTY4NDU2Nzk0.Xr_hhQ.15ehTql1mlptRCgPThshcxctmqQ')
+dotenv.config()
+client.login(process.env.TOKEN)
 console.log('Discord client login')
